25481ae7efa7c36ccf2dde52553177c65fc6ad77
Ashwini18-tech <ashwinimaher123@gmail.com>
2025-07-11 19:18:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TextToVoice from "./component/TextToVoice.jsx";
+import "./component/TextToVoice.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+function App() {
+    return <TextToVoice />;
+}
+
+export default App;
